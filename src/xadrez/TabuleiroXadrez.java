b90b1156502b9252a94be55ee0b38251cdc41b60
Felipe <fpachecomanoel@uol.com.br>
2020-04-28 22:41:07 -0300

    mudancas na Peca , tabuleiro e adicao da dama
    adicionei alguns atributos na peca e criei funcoes mover(wip) e set no tabuleiro e criei o movimento da dama na diagonal

--- src/xadrez/TabuleiroXadrez.java
+++ src/xadrez/TabuleiroXadrez.java
@@ -21,5 +21,9 @@ public class TabuleiroXadrez {
 		String pos=""+((char)('a'+vet[1]))+((char)((7-vet[0])+'1'));
 		return pos;
 	}
+	void set(String pos,Peca obj) {
+		int[] vet=convertString(pos);
+		tab[vet[0]][vet[1]]=obj;
+	}
 }
 
