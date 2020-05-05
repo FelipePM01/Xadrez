df6d2fa3007bb6331396840d486f5dfd458f0182
Felipe <fpachecomanoel@uol.com.br>
2020-05-05 13:48:34 -0300

    correcao do merge

--- src/xadrez/Cavalo.java
+++ src/xadrez/Cavalo.java
@@ -11,14 +11,14 @@ public class Cavalo extends Peca{
         int[] vetDestino=tabuleiro.convertString(destino);
         //Verifica se o destino recebido respeita o movimento do cavalo
         if((tabuleiro.turno=="brancas"&&Character.isLowerCase(this.tipo))||(tabuleiro.turno=="pretas"&&Character.isUpperCase(this.tipo))) {
-        if((Math.abs(vetOrigem[0]-vetDestino[0])==2&&Math.abs(vetOrigem[1]-vetDestino[1])==1)||(Math.abs(vetOrigem[0]-vetDestino[0])==1&&Math.abs(vetOrigem[1]-vetDestino[1])==2)){
-            Peca obj=tabuleiro.getPeca(vetDestino[0],vetDestino[1]);
-            if(obj==null||(Character.isLowerCase(obj.tipo)!=Character.isLowerCase(this.tipo))){
-                tabuleiro.set(posicao,null);
-                tabuleiro.set(destino,this);
-                this.posicao = destino;
-            }
+	        if((Math.abs(vetOrigem[0]-vetDestino[0])==2&&Math.abs(vetOrigem[1]-vetDestino[1])==1)||(Math.abs(vetOrigem[0]-vetDestino[0])==1&&Math.abs(vetOrigem[1]-vetDestino[1])==2)){
+	            Peca obj=tabuleiro.getPeca(vetDestino[0],vetDestino[1]);
+	            if(obj==null||(Character.isLowerCase(obj.tipo)!=Character.isLowerCase(this.tipo))){
+	                tabuleiro.set(posicao,null);
+	                tabuleiro.set(destino,this);
+	                this.posicao = destino;
+	            }
+	        }
         }
-    }
     } 
 }
